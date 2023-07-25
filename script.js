406d98a40b0faa02d2303cc5c89fd0da3e281b2b
SwarnaSkyla <117882181+SwarnaSkyla@users.noreply.github.com>
2023-07-25 21:50:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+document.getElementById("myForm").addEventListener("submit",function(event){
+	event.preventDefault();
+
+	const name=document.getElementById("name").value;
+	const year=document.getElementById("year").value;
+
+	const queryString=`name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+
+	document.getElementById("url").textContent="https://localhost:8080/"+queryString;
+});
